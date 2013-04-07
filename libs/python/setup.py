3d3e7a3afeba72795ede44d802d4929790e0e1b5
Alonso Vidales <alonso.vidales@tras2.es>
2013-04-08 00:44:44 +0200

    Working on the installation process and the makefile

---
+++ libs/python/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import os
+
+from brain import __version__
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='brain',
+    version=__version__,
+    description='Python client for Brain - On memory high performance distributed storage system',
+    url='https://github.com/alonsovidales/Brain2',
+    author='Alonso Vidales',
+    author_email='alonso.vidales@tras2.es',
+    maintainer='Alonso Vidales',
+    maintainer_email='alonso.vidales@tras2.es',
+    keywords=['Brain', 'key-value store', 'cache'],
+    license='MIT',
+    packages=['brain'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ]
+)
